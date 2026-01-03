41a54e51eed318c6cb7d6e26f711a05186c13bf3
Mrestof <mrestof@disroot.org>
2026-01-03 09:13:41 +0100

    complete 5-1

--- src/ex51.c
+++ src/ex51.c
@@ -34,8 +34,13 @@ int getint(int *pn) {
     return 0;
   }
   sign = (c == '-') ? -1 : 1;
-  if (c == '+' || c == '-')
+  if (c == '+' || c == '-') {
     c = getch();
+    if (!isdigit(c)) {
+      ungetch(c);
+      return 0;
+    }
+  }
   for (*pn = 0; isdigit(c); c = getch())
     *pn = 10 * *pn + (c - '0');
   *pn *= sign;
