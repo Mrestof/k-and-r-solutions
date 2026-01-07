f7794b27809970fa128ea8a4d2e5fa0db6093a0e
Mrestof <mrestof@disroot.org>
2026-01-07 10:34:10 +0100

    add ANSI color codes for easy access

--- src/lib/myutils.h
+++ src/lib/myutils.h
@@ -3,6 +3,16 @@
 
 #include <stdbool.h>
 
+#define BLK "\033[0;30m"
+#define RED "\033[0;31m"
+#define GRN "\033[0;32m"
+#define YEL "\033[0;33m"
+#define BLU "\033[0;34m"
+#define MAG "\033[0;35m"
+#define CYN "\033[0;36m"
+#define WHT "\033[0;37m"
+#define RST "\033[0m"
+
 int my_rand(void);
 void my_srand(unsigned long seed);
 
