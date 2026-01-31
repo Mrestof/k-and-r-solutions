0462b480a543e7921b4ed27bc3c9b8139c6a6d1d
Mrestof <mrestof@disroot.org>
2026-01-31 06:55:56 +0100

    add base for 5-15 from 5-14 with tests

---
+++ src/ex515.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXLINES 5000   /* max #lines to be sorted */
+char *lineptr[MAXLINES]; /* pointers to text lines */
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void kr_qsort(
+  void *lineptr[], int left, int right, int reverse,
+  int (*comp)(void *, void *)
+);
+int numcmp(const char *, const char *);
+
+/* sort input lines */
+int main(int argc, char *argv[])
+{
+  int nlines;    /* number of input lines read */
+  int numeric = 0;   /* 1 if numeric sort */
+  int reverse = 1;   /* -1 if reverse sort */
+
+  while (--argc > 0 && **++argv == '-')
+    while (*++*argv)
+      switch (**argv) {
+      case 'n':
+        numeric = 1;
+        break;
+      case 'r':
+        reverse = -1;
+        break;
+      default:
+        return 2;
+      }
+
+  if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+    kr_qsort(
+      (void**) lineptr, 0, nlines-1, reverse,
+      (int (*)(void*,void*))(numeric ? numcmp : strcmp)
+    );
+    writelines(lineptr, nlines);
+    return 0;
+  } else {
+    printf("input too big to sort\n");
+    return 1;
+  }
+}
+
+/* kr_qsort: sort v[left]...v[right] into increasing order */
+void kr_qsort(
+  void *v[], int left, int right, int reverse,
+  int (*comp)(void *, void *)
+) {
+  int i, last;
+  void arr_swap(void *v[], int, int);
+
+  if (left >= right)  /* do nothing if array contains */
+    return;       /* fewer than two elements */
+  arr_swap(v, left, (left + right)/2);
+  last = left;
+  for (i = left+1; i <= right; i++)
+    if (reverse * (*comp)(v[i], v[left]) < 0)
+      arr_swap(v, ++last, i);
+  arr_swap(v, left, last);
+  kr_qsort(v, left, last-1, reverse, comp);
+  kr_qsort(v, last+1, right, reverse, comp);
+}
+
+/* numcmp: compare s1 and s2 numerically */
+int numcmp(const char *s1, const char *s2)
+{
+  double v1, v2;
+
+  v1 = atof(s1);
+  v2 = atof(s2);
+  if (v1 < v2)
+    return -1;
+  else if (v1 > v2)
+    return 1;
+  else
+    return 0;
+}
+
+void arr_swap(void *v[], int i, int j)
+{
+  void *temp;
+
+  temp = v[i];
+  v[i] = v[j];
+  v[j] = temp;
+}
